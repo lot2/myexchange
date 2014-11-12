a7ecf42410e244e44d960abb74417b74de6337b0
lot.xu <lot.xu@apeksys.com>
2014-11-12 11:35:19 +0800

    20141112 login html

--- apps/index.py
+++ apps/index.py
@@ -7,4 +7,25 @@ from apps import app
 @app.route('/')
 @app.route('/index')
 def index():
+    if 'username' not in session:
+        return redirect(url_for('login'))
     return render_template("index.html", title='Home')
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        session['username'] = request.form['username']
+        return redirect(url_for('index'))
+    return render_template("login.html", title='Login')
+
+
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    return redirect(url_for('index'))
+
+
+@app.route('/session')
+def get_all_session():
+    return  render_template("get_sessions.html")
